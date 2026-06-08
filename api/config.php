804f4a1f967446726a52917ea17422830a9214f5
bintangrangga <bintangrangga2006@gmail.com>
2026-06-09 00:24:22 +0700

    second commit

--- api/config.php
+++ api/config.php
@@ -8,7 +8,7 @@ $db   = "manajemen_kegiatan";
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
+    die("Koneksi ke database failed: " . mysqli_connect_error());
 }
 
 define('BASE_URL', 'http://10.21.3.195/manajemen_kegiatan');
